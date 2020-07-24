2057dc2d47ea2fe1d130d9691185da2bb1af8170
Paul Abel Sanchez <psanchez@oriontek.do>
2020-07-24 17:15:45 -0400

    Anotaciones

--- Proyecto Noticias/Models/Noticia.cs
+++ Proyecto Noticias/Models/Noticia.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,17 @@ namespace Proyecto_Noticias.Models
     public class Noticia
     {
         public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
+        [Required]
         public string Resumen { get; set; }
+        [Required]
         public string Url { get; set; }
         public int Like { get; set; }
         public int Dislike { get; set; }
+        [Required]
         public DateTime Fecha { get; set; }
+        [Required]
         public string Categoria { get; set; }
 
         public List<Comentario> Comentarios { get; set; }
